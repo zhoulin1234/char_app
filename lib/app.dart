dfa44ab232269729928800834c93e7cd11987250
zhoulin <zhoulin@163.com>
2019-11-10 22:47:45 +0800

    增加消息页面 个人页面 以及我的好友页面

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,6 @@
+import 'package:app_test/chat/message_page.dart';
+import 'package:app_test/contacts/contacts.dart';
+import 'package:app_test/personal/personal.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
@@ -7,6 +10,22 @@ class App extends StatefulWidget {
 
 //应用页面状态实现类
 class _AppState extends State<App> {
+  //当前选中页的索引
+  var _currentIndex = 0;
+  MessagePage message;
+  Contacts contacts;
+  Personal me;
+
+  /**
+   * 根据不同的索引返回不同的页面
+   */
+  currentPage() {
+    switch(_currentIndex){
+      case 0:
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/personal/personal.dart
+++ lib/personal/personal.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-class MessagePage extends StatelessWidget {
+
+class Personal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+
