11a4b13ad9a8a77b39002e4a93af78f4a9b2db54
zhoulin <zhoulin@realcan.cn>
2019-11-11 16:43:48 +0800

    好友列表 个人信息功能添加

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
+//加载页面
 class LoadingPage extends StatefulWidget {
   @override
-  _LoadingPageState createState() => _LoadingPageState();
+  _LoadingState createState() => _LoadingState();
 }
 
-class _LoadingPageState extends State<LoadingPage> {
+class _LoadingState extends State<LoadingPage> {
+
   @override
-  void initState() {
+  void initState(){
     super.initState();
-    new Future.delayed(Duration(seconds: 3), () {
+    //在加载页面停顿3秒
+    Future.delayed(Duration(seconds: 3),(){
+      print("Flutter即时通讯APP界面实现....");
       Navigator.of(context).pushReplacementNamed("app");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Center(
+    return Center(
       child: Stack(
         children: <Widget>[
-          Image.asset(
-            "images/loading.jpeg",
-            fit: BoxFit.cover,
-          )
+          //加载页面居中背景图 使用cover模式
+          Image.asset("images/loading.jpeg",fit: BoxFit.cover,),
         ],
       ),
     );
   }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,35 @@
-import 'package:app_test/app.dart';
-import 'package:app_test/loading.dart';
 import 'package:flutter/material.dart';
+import './app.dart';
+import './loading.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import './search.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: '来聊天啊',
-        theme: myDefaultTheme,
-        home: new LoadingPage(),
-        routes: <String, WidgetBuilder>{
-          "app": (BuildContext context) => new App(),
-          "/friends": (_) => new WebviewScaffold(
-                url: "http://www.realcan.cn/",
-                appBar: new AppBar(
-                  title: Text('realcan官网'),
-                ),
-                withZoom: true,
-                withLocalStorage: true,
+//应用程序入口
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '聊天室',
+      //自定义主题
+      theme: mDefaultTheme,
+      //添加路由表
+      routes: <String, WidgetBuilder>{
+        "app": (BuildContext context) => App(),
+        "/friends": (_) => WebviewScaffold(
+              //Webview插件
+              url: "https://flutter.io/",
+              appBar: AppBar(
+                title: Text("Flutter官网"),
               ),
-        });
-  }
-}
+              withZoom: true,
+              withLocalStorage: true,
+            ),
+        'search': (BuildContext context) => Search(), //搜索页面路由
+      },
+      //指定首页 默认为加载页面
+      home: LoadingPage(),
+    ));
 
-//自定义主题
-final ThemeData myDefaultTheme = new ThemeData(
+//自定义主题 绿色小清新风格
+final ThemeData mDefaultTheme = ThemeData(
   primaryColor: Colors.green,
   scaffoldBackgroundColor: Color(0xFFebebeb),
   cardColor: Colors.green,
